19dae1a14893e8d0b5f439094e1f9a156a0f8495
manager <manager@163.com>
2015-10-31 02:16:47 +0800

    加载AccessToken后添加回调

---
+++ weibo 1/Class/Module(模块)/Profile(我)/CZNetwoorkTools.swift
@@ -0,0 +1,13 @@
+//
+//  CZNetwoorkTools.swift
+//  weibo 1
+//
+//  Created by sa on 15/10/30.
+//  Copyright © 2015年 sa. All rights reserved.
+//
+
+import UIKit
+
+class CZNetwoorkTools: NSObject {
+
+}
