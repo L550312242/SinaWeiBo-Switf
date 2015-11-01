15af9baef928acd0c1c0698ccf5efa535382d5c5
manager <manager@163.com>
2015-11-01 19:43:14 +0800

    add file

---
+++ weibo 1/Class/Module(模块)/NewFeature(新特性)/Controller/CZWelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CZWelcomeViewController.swift
+//  weibo 1
+//
+//  Created by sa on 15/11/1.
+//  Copyright © 2015年 sa. All rights reserved.
+//
+
+import UIKit
+
+class CZWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
